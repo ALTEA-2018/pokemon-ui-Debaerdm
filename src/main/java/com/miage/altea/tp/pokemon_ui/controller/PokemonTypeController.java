711f5e860bf56189807a9d2a11522d98efdbb511
Mathieu De Baerdemaeker <mathieu.debaerdemaeker@gmail.com>
2019-02-11 14:27:07 +0100

    feat: fini

---
+++ src/main/java/com/miage/altea/tp/pokemon_ui/controller/PokemonTypeController.java
@@ -0,0 +1,33 @@
+package com.miage.altea.tp.pokemon_ui.controller;
+
+import com.miage.altea.tp.pokemon_ui.pokemonTypes.bo.PokemonType;
+import com.miage.altea.tp.pokemon_ui.pokemonTypes.service.PokemonTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class PokemonTypeController {
+
+    private PokemonTypeService pokemonTypeService;
+
+    @GetMapping("/pokedex")
+    public ModelAndView pokedex(){
+        Map<String, Object> stringObjectMap = new HashMap<>();
+        List<PokemonType> pokemonTypes = pokemonTypeService.listPokemonsTypes();
+        pokemonTypes.sort(Comparator.comparing(PokemonType::getId));
+        stringObjectMap.put("pokemonTypes", pokemonTypes);
+        return new ModelAndView("pokedex", stringObjectMap);
+    }
+
+    @Autowired
+    public void setPokemonTypeService(PokemonTypeService pokemonTypeService) {
+        this.pokemonTypeService = pokemonTypeService;
+    }
+}
---
+++ src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -0,0 +1,49 @@
+package com.miage.altea.tp.pokemon_ui.pokemonTypes.service;
+
+import com.miage.altea.tp.pokemon_ui.pokemonTypes.bo.PokemonType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class PokemonTypeServiceImpl implements PokemonTypeService {
+
+    private RestTemplate restTemplate;
+    private String url;
+
+    @Override
+    public List<PokemonType> listPokemonsTypes() {
+        PokemonType[] objects = this.restTemplate.getForObject(this.url+"/pokemon-types/", PokemonType[].class);
+
+        if (objects != null) {
+            return Arrays.asList(objects);
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    @Override
+    public PokemonType getPokemonByTypeId(Integer id) {
+        PokemonType pokemonType = this.restTemplate.getForObject(this.url+"/pokemon-types/", PokemonType.class, id);
+
+        if (pokemonType != null) {
+            return pokemonType;
+        } else {
+            throw new NullPointerException();
+        }
+    }
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${pokemonType.service.url}")
+    public void setPokemonTypeServiceUrl(String pokemonServiceUrl) {
+        this.url = pokemonServiceUrl;
+    }
+}

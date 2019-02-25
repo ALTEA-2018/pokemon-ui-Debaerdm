d0daa75364cfb5755f4f9d53561e0d6d189acf5c
Mathieu De Baerdemaeker <mathieu.debaerdemaeker@gmail.com>
2019-02-25 11:46:56 +0100

    fix: language

--- src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
+++ src/main/java/com/miage/altea/tp/pokemon_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -3,9 +3,17 @@ package com.miage.altea.tp.pokemon_ui.pokemonTypes.service;
 import com.miage.altea.tp.pokemon_ui.pokemonTypes.bo.PokemonType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,7 +36,7 @@ public class PokemonTypeServiceImpl implements PokemonTypeService {
 
     @Override
     public PokemonType getPokemonByTypeId(Integer id) {
-        PokemonType pokemonType = this.restTemplate.getForObject(this.url+"/pokemon-types/", PokemonType.class, id);
+        PokemonType pokemonType = this.restTemplate.getForObject(this.url+"/pokemon-types", PokemonType.class, id);
 
         if (pokemonType != null) {
             return pokemonType;
